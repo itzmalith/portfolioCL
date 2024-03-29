6b07f0af33ccdb6fcc347fded07902b0c7cb5609
Malith <[14:35] Lasanga Dissanayakemalith.lekamge@atlinkcom.com>
2024-03-29 18:05:15 +0530

    intial upload nethuni portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry)=>{
+    console.log(entry);
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+
+    }
+
+  })
+
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+window.onload = function() {
+  document.querySelector('.title').style.animationPlayState = 'running';
+};
+
